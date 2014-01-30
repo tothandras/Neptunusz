af1ddfb1dec6a9e60d0c2f1f91b73f7e0b715334
Andrew Szell <coldcue@gmail.com>
2014-01-30 17:17:06 +0100

    sort changed to reverse

--- src/main/java/com/neptunusz/model/service/SubjectService.java
+++ src/main/java/com/neptunusz/model/service/SubjectService.java
@@ -37,7 +37,7 @@ public class SubjectService {
     }
 
     public List<Subject> getSubjects() {
-        Collections.sort(subjects);
+        Collections.reverse(subjects);
         return subjects;
     }
 
